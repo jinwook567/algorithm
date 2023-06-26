4574fda3652b76812b69b6a88a71e6c89283c1c9
jinwook567 <jinwook567@we2d.com>
2023-06-26 16:34:38 +0900

    큐스택데크/짝이맞지않은괄호

--- 문제/알문해전/큐스택데크/짝이맞지않은괄호/ans.js
+++ 문제/알문해전/큐스택데크/짝이맞지않은괄호/ans.js
@@ -1,4 +1,23 @@
-function solution(example) {}
+function solution(example) {
+  const stack = [];
 
+  const open = ["(", "{", "["];
+  const close = [")", "}", "]"];
+
+  for (let i = 0; i < example.length; i++) {
+    console.log(stack);
+    if (open.includes(example[i])) stack.push(example[i]);
+    else {
+      const closeIndex = close.findIndex((v) => v === example[i]);
+      const last = stack.pop();
+      const openIndex = open.findIndex((v) => v === last);
+      if (closeIndex !== openIndex) return "NO";
+    }
+  }
+
+  return stack.length === 0 ? "YES" : "NO";
+}
+
+const example = "({}[(){}])";
 const r = solution(example);
 console.log(r);
